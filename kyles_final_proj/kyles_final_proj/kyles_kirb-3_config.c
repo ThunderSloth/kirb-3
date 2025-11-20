6e809a75c1d0e1903b68327364d838b507a641ca
KyleOtto <kyle_otto101@yahoo.com>
2025-11-20 12:34:15 -0500

    Power init funtion working

--- kyles_final_proj/kyles_final_proj/kyles_kirb-3_config.c
+++ kyles_final_proj/kyles_final_proj/kyles_kirb-3_config.c
@@ -8,6 +8,7 @@
 // Loads MSP launchpad board support macros and definitions
 //-----------------------------------------------------------------------------
 
+#include "ti/devices/msp/m0p/mspm0g350x.h"
 #include "ti/driverlib/dl_timera.h"
 #include "ti/driverlib/dl_timerg.h"
 #include "ti_msp_dl_config.h"
@@ -67,42 +68,84 @@ void config_init(void)
 
 
 
-power_init()
+void power_init(void)
 {
+     // Reset two GPIO peripherals
+    // DL_GPIO_reset(GPIOA);
+    GPIOA->GPRCM.RSTCTL = (GPIO_RSTCTL_KEY_UNLOCK_W |
+                            GPIO_RSTCTL_RESETSTKYCLR_CLR |
+                            GPIO_RSTCTL_RESETASSERT_ASSERT);
+    //DL_GPIO_reset(GPIOB);
+    GPIOB->GPRCM.RSTCTL = (GPIO_RSTCTL_KEY_UNLOCK_W |
+                            GPIO_RSTCTL_RESETSTKYCLR_CLR |
+                            GPIO_RSTCTL_RESETASSERT_ASSERT);
 
+    //   DL_TimerA_reset(MOTOR_PWM_INST);
+    MOTOR_PWM_INST->GPRCM.RSTCTL = (GPTIMER_RSTCTL_KEY_UNLOCK_W | 
+                                    GPTIMER_RSTCTL_RESETSTKYCLR_CLR |
+                                    GPTIMER_RSTCTL_RESETASSERT_ASSERT);
+
+    //    DL_TimerA_reset(RC_TIM0_INST);
+    RC_TIM0_INST->GPRCM.RSTCTL = (GPTIMER_RSTCTL_KEY_UNLOCK_W | 
+                                    GPTIMER_RSTCTL_RESETSTKYCLR_CLR |
+                                    GPTIMER_RSTCTL_RESETASSERT_ASSERT);
+
+    //   DL_TimerG_reset(RC_TIM1_INST);
+    RC_TIM1_INST->GPRCM.RSTCTL = (GPTIMER_RSTCTL_KEY_UNLOCK_W | 
+                                    GPTIMER_RSTCTL_RESETSTKYCLR_CLR |
+                                    GPTIMER_RSTCTL_RESETASSERT_ASSERT);
+
+    // Enable power to two GPIO peripherals
+    GPIOA->GPRCM.PWREN = (GPIO_PWREN_KEY_UNLOCK_W | GPIO_PWREN_ENABLE_ENABLE);
+    GPIOB->GPRCM.PWREN = (GPIO_PWREN_KEY_UNLOCK_W | GPIO_PWREN_ENABLE_ENABLE);
+
+    //DL_TimerA_enablePower(MOTOR_PWM_INST);
+    MOTOR_PWM_INST->GPRCM.PWREN = (GPTIMER_PWREN_KEY_UNLOCK_W | GPTIMER_PWREN_ENABLE_ENABLE);
+
+    // DL_TimerA_enablePower(RC_TIM0_INST);
+    RC_TIM0_INST->GPRCM.PWREN = (GPTIMER_PWREN_KEY_UNLOCK_W | GPTIMER_PWREN_ENABLE_ENABLE);
+
+    // DL_TimerG_enablePower(RC_TIM1_INST);
+    RC_TIM1_INST->GPRCM.PWREN = (GPTIMER_PWREN_KEY_UNLOCK_W | GPTIMER_PWREN_ENABLE_ENABLE);
+
+    clock_delay(24);
 }
 
 
 
-GPIO_init()
-{
-
-}
-
-
-
-clock_init();
-{
-
-}
-
-
-
-PWM_init()
-{
-
-}
-
-
-
-RC_timer0_init()
-{
-
-}
-
-
-
-RC_timer1_init()
+void GPIO_init(void)
 {
     
 }
+
+
+
+void clock_init(void);
+{
+
+}
+
+
+
+void PWM_init(void)
+{
+
+}
+
+
+
+void RC_timer0_init(void)
+{
+
+}
+
+
+
+void RC_timer1_init(void)
+{
+
+}
+
+
+
+
