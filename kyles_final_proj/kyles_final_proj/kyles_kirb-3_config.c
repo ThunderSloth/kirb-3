1f46430b507f72e1d5a30d4a02acb76b6ee3bb4e
KStrowbridge <kylestrow@gmail.com>
2025-11-25 09:56:50 -0500

    Wrote GPIO init function

--- kyles_final_proj/kyles_final_proj/kyles_kirb-3_config.c
+++ kyles_final_proj/kyles_final_proj/kyles_kirb-3_config.c
@@ -32,6 +32,37 @@ void RC_timer1_init(void);
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
 
+
+// GPIO_init() constants
+//------------------------------------------------------------------------------
+#define GPIO_MOTOR_PWM_C0_IOMUX                                  (IOMUX_PINCM15)
+#define GPIO_MOTOR_PWM_C0_IOMUX_FUNC                 IOMUX_PINCM15_PF_TIMA1_CCP0
+#define GPIO_MOTOR_PWM_C0_PIN                                       (0x00000004)
+
+#define GPIO_MOTOR_PWM_C1_IOMUX                                  (IOMUX_PINCM16)
+#define GPIO_MOTOR_PWM_C1_IOMUX_FUNC                 IOMUX_PINCM16_PF_TIMA1_CCP1
+#define GPIO_MOTOR_PWM_C1_PIN                                       (0x00000008)
+
+#define GPIO_RC_TIM0_C0_IOMUX                                    (IOMUX_PINCM19)
+#define GPIO_RC_TIM0_C0_IOMUX_FUNC                   IOMUX_PINCM19_PF_TIMA0_CCP0
+
+#define GPIO_RC_TIM0_C2_IOMUX                                    (IOMUX_PINCM37)
+#define GPIO_RC_TIM0_C2_IOMUX_FUNC                   IOMUX_PINCM37_PF_TIMA0_CCP2
+
+#define GPIO_RC_TIM0_C3_IOMUX                                    (IOMUX_PINCM55)
+#define GPIO_RC_TIM0_C3_IOMUX_FUNC                   IOMUX_PINCM55_PF_TIMA0_CCP3
+
+#define GPIO_RC_TIM1_C0_IOMUX                                     (IOMUX_PINCM3)
+#define GPIO_RC_TIM1_C0_IOMUX_FUNC                    IOMUX_PINCM3_PF_TIMG7_CCP0
+
+#define RC_IN_CH5_IOMUX                                          (IOMUX_PINCM30)
+#define RC_IN_CH6_IOMUX                                          (IOMUX_PINCM48)
+
+#define RC_IN_PORT                                                       (GPIOB)
+
+#define RC_IN_CH5_PIN                                               (0x00002000)
+#define RC_IN_CH6_PIN                                               (0x00100000)
+//------------------------------------------------------------------------------
 // testing
 /*
 
@@ -114,6 +145,7 @@ void power_init(void)
 
 void GPIO_init(void)
 {
+  //Constants still need to be defined
 
   //Configure PWM motor output (TIMA1_C0) on pin PB2 
   IOMUX->SECCFG.PINCM[GPIO_MOTOR_PWM_C0_IOMUX] = GPIO_MOTOR_PWM_C0_IOMUX_FUNC | IOMUX_PINCM_PC_CONNECTED;
@@ -125,11 +157,11 @@ void GPIO_init(void)
   //Enable output on pin PB3
   GPIO_MOTOR_PWM_C1_PORT->DOESET31_0 = GPIO_MOTOR_PWM_C1_PIN;
 
-  //Configure RC input (TIMA0_C0N) on pin PA8 
+  //Configure RC input (TIMA0_C0) on pin PA8 
   IOMUX->SECCFG.PINCM[GPIO_RC_TIM0_C0_IOMUX] =
       GPIO_RC_TIM0_C0_IOMUX_FUNC | IOMUX_PINCM_PC_CONNECTED | IOMUX_PINCM_INENA_ENABLE;
 
-  //Configure RC input (TIM0_C2N) on pin PA15 
+  //Configure RC input (TIM0_C2) on pin PA15 
   IOMUX->SECCFG.PINCM[GPIO_RC_TIM0_C2_IOMUX] =
       GPIO_RC_TIM0_C2_IOMUX_FUNC | IOMUX_PINCM_PC_CONNECTED | IOMUX_PINCM_INENA_ENABLE;
 
@@ -141,25 +173,59 @@ void GPIO_init(void)
   IOMUX->SECCFG.PINCM[GPIO_RC_TIM1_C0_IOMUX] =
       GPIO_RC_TIM1_C0_IOMUX_FUNC | IOMUX_PINCM_PC_CONNECTED | IOMUX_PINCM_INENA_ENABLE;
 
+  //Configure pin PB13 as a GPIO input
+  /* GPIO functionality is always a pin function of 0x00000001 */
+  IOMUX->SECCFG.PINCM[RC_IN_CH5_IOMUX] =
+      IOMUX_PINCM_INENA_ENABLE | IOMUX_PINCM_PC_CONNECTED |
+      ((uint32_t) 0x00000001) | (uint32_t) IOMUX_PINCM_INV_DISABLE |
+      (uint32_t) (IOMUX_PINCM_PIPU_DISABLE | IOMUX_PINCM_PIPD_DISABLE) | (uint32_t) IOMUX_PINCM_HYSTEN_DISABLE |
+      ((uint32_t) IOMUX_PINCM_WUEN_DISABLE & IOMUX_PINCM_WCOMP_MASK);
+  IOMUX->SECCFG.PINCM[RC_IN_CH5_IOMUX] |=
+      ((uint32_t) IOMUX_PINCM_WUEN_DISABLE & IOMUX_PINCM_WUEN_MASK);
+
+  //Configure pin PB20 as a GPIO input
+  /* GPIO functionality is always a pin function of 0x00000001 */
+  IOMUX->SECCFG.PINCM[RC_IN_CH6_IOMUX] =
+      IOMUX_PINCM_INENA_ENABLE | IOMUX_PINCM_PC_CONNECTED |
+      ((uint32_t) 0x00000001) | (uint32_t) IOMUX_PINCM_INV_DISABLE |
+      (uint32_t) (IOMUX_PINCM_PIPU_DISABLE | IOMUX_PINCM_PIPD_DISABLE) | (uint32_t) IOMUX_PINCM_HYSTEN_DISABLE |
+      ((uint32_t) IOMUX_PINCM_WUEN_DISABLE & IOMUX_PINCM_WCOMP_MASK);
+  IOMUX->SECCFG.PINCM[RC_IN_CH6_IOMUX] |=
+      ((uint32_t) IOMUX_PINCM_WUEN_DISABLE & IOMUX_PINCM_WUEN_MASK);
+    
+  //Sets ploarity for lower bits
+  RC_IN_PORT->POLARITY15_0 |= GPIO_POLARITY15_0_DIO13_FALL;
+  //Sets polarity for upper bits
+  RC_IN_PORT->POLARITY31_16 |= GPIO_POLARITY31_16_DIO20_FALL;
+  //Clears interrupts
+  RC_IN_PORT->CPU_INT.ICLR |= (RC_IN_CH5_PIN | RC_IN_CH6_PIN);
+  //Enables interrupts
+  RC_IN_PORT->CPU_INT.IMASK |= (RC_IN_CH5_PIN |RC_IN_CH6_PIN);
+
+
 }
 
 
 
 void clock_init(void)
 {
+  SYSCTL->SOCLOCK.BORTHRESHOLD = (uint32_t) SYSCTL_BORTHRESHOLD_LEVEL_BORMIN;
 
+  
 }
 
 
 
 void PWM_init(void)
 {
-  IOMUX->SECCFG.PINCM[GPIO_MOTOR_PWM_C0_IOMUX] = GPIO_MOTOR_PWM_C0_IOMUX_FUNC | IOMUX_PINCM_PC_CONNECTED;
+  gptimer->CLKSEL = (uint32_t)(config->clockSel);
 
-  GPIO_MOTOR_PWM_C0_PORT->DOESET31_0 = GPIO_MOTOR_PWM_C0_PIN;
+  gptimer->CLKDIV = (uint32_t)(config->divideRatio);
+
+  gptimer->COMMONREGS.CPS = (config->prescale);
 }  
 
-}
+
 
 
 
