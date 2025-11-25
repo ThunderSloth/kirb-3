21c043909091c0ae4591a44bc0239e9133c38563
KyleOtto <kyle_otto101@yahoo.com>
2025-11-25 09:59:12 -0500

    file changes

---
+++ kyles_final_proj/kyles_final_proj/Debug/Debug copy/subdir_vars.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../Debug\ copy/device_linker.cmd 
+
+O_SRCS += \
+../Debug\ copy/kyles_kirb-3.o \
+../Debug\ copy/startup_mspm0g350x_ticlang.o \
+../Debug\ copy/ti_msp_dl_config.o 
+
+OPT_SRCS += \
+../Debug\ copy/ccsObjs.opt \
+../Debug\ copy/device.opt 
+
+OUT_SRCS += \
+../Debug\ copy/kyles_final_proj.out 
+
+C_SRCS += \
+../Debug\ copy/ti_msp_dl_config.c 
+
+C_DEPS += \
+./Debug\ copy/ti_msp_dl_config.d 
+
+OBJS += \
+./Debug\ copy/ti_msp_dl_config.o 
+
+OBJS__QUOTED += \
+"Debug copy\ti_msp_dl_config.o" 
+
+C_DEPS__QUOTED += \
+"Debug copy\ti_msp_dl_config.d" 
+
+OPT_SRCS__QUOTED += \
+"../Debug copy/ccsObjs.opt" \
+"../Debug copy/device.opt" 
+
+OUT_SRCS__QUOTED += \
+"../Debug copy/kyles_final_proj.out" 
+
+C_SRCS__QUOTED += \
+"../Debug copy/ti_msp_dl_config.c" 
+
+
---
+++ kyles_final_proj/kyles_final_proj/Debug/targetConfigs/Debug/subdir_vars.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../targetConfigs/Debug/device_linker.cmd 
+
+O_SRCS += \
+../targetConfigs/Debug/kyles_kirb-3.o \
+../targetConfigs/Debug/startup_mspm0g350x_ticlang.o \
+../targetConfigs/Debug/ti_msp_dl_config.o 
+
+OPT_SRCS += \
+../targetConfigs/Debug/ccsObjs.opt \
+../targetConfigs/Debug/device.opt 
+
+OUT_SRCS += \
+../targetConfigs/Debug/kyles_final_proj.out 
+
+C_SRCS += \
+../targetConfigs/Debug/ti_msp_dl_config.c 
+
+C_DEPS += \
+./targetConfigs/Debug/ti_msp_dl_config.d 
+
+OBJS += \
+./targetConfigs/Debug/ti_msp_dl_config.o 
+
+OBJS__QUOTED += \
+"targetConfigs\Debug\ti_msp_dl_config.o" 
+
+C_DEPS__QUOTED += \
+"targetConfigs\Debug\ti_msp_dl_config.d" 
+
+OPT_SRCS__QUOTED += \
+"../targetConfigs/Debug/ccsObjs.opt" \
+"../targetConfigs/Debug/device.opt" 
+
+OUT_SRCS__QUOTED += \
+"../targetConfigs/Debug/kyles_final_proj.out" 
+
+C_SRCS__QUOTED += \
+"../targetConfigs/Debug/ti_msp_dl_config.c" 
+
+
